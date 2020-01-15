3e5bfeaefadbfa76dfbe032f3d5a58d30a0334ba
Akshay K <akshaykunti1@gmail.com>
2020-01-15 16:08:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   
   @@count = 
   @@artists = []
+  @@genres = []
   attr_accessor :name, :artist , :genre 
   
   def initialize(name, artist, genre)
@@ -9,18 +10,20 @@ class Song
     @artist = artist
     @genre = genre
     @@count +=1
+    @@artists << artist
+    @@genres << genre
   end
   
   def artists 
-    
+    @@artists
   end
   
   def genre_count
-  
+    @@genres.length
   end
   
   def count
-    
+    @@count
   end
     
   
