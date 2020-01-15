c59e4c43f56ce52643b577215cea9689f92aef21
Akshay K <akshaykunti1@gmail.com>
2020-01-15 16:12:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,54 @@
-class Song 
-  
-  @@count = 
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
   @@artists = []
   @@genres = []
-  attr_accessor :name, :artist , :genre 
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count +=1
+    @@count += 1
     @@artists << artist
     @@genres << genre
   end
-  
-  def artists 
-    @@artists
-  end
-  
-  def genre_count
-    @@genres.length
-  end
-  
-  def count
+
+  def self.count
     @@count
   end
-    
-  
+
+  def self.artists
+    @value = @@artists
+    @value.uniq
+  end
+
+  def self.artist_count
+    value = {}
+    @@artists.each do |array_value|
+      if value[array_value]
+        value[array_value] += 1
+      else
+        value[array_value] = 1
+      end
+    end
+    value
+  end
+
+  def self.genres
+    @value = @@genres
+    @value.uniq
+  end
+
+  def self.genre_count
+    value = {}
+    @@genres.each do |array_value|
+      if value[array_value]
+        value[array_value] += 1
+      else
+        value[array_value] = 1
+      end
+    end
+    value
+  end
 end
